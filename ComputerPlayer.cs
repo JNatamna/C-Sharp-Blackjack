ae819b929c84014e3424a70657235a34c0d1b1b2
JNatamna <32371492+JNatamna@users.noreply.github.com>
2022-11-01 15:27:07 +0100

    Add files via upload

---
+++ ComputerPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Blackjack
+{
+    class ComputerPlayer : Player
+    {
+        public ComputerPlayer(GameBoard game)
+            : base(game)
+        {
+
+        }
+
+        protected override bool ShouldDrawAnotherCard()
+        {
+            return CurrentTotal < 17;
+        }
+
+        protected override void DrawCard()
+        {
+            base.DrawCard();
+            Console.WriteLine("Dealer pulled \"{0}\" from the deck. Dealer's current total is {1}", LatestDrawnCard, CurrentTotal);
+        }
+    }
+}
